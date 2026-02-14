5779acbea8b0fea06379a52907a7df2212befe58
Leonel Cortés <leobardo.cortes4751@alumnos.udg.mx>
2026-02-14 11:27:43 -0600

    Refactor HTML structure and enhance responsiveness; update animation logic for card toggle

--- src/js/animacion.js
+++ src/js/animacion.js
@@ -1,11 +1,30 @@
 $(document).ready(function () {
-    $('.valentines').mouseenter(function () {
-        $('.card').stop().animate({
-            top: '-90px'
-        }, 'slow');
-    }).mouseleave(function () {
-        $('.card').stop().animate({
-            top: 0
-        }, 'slow');
+
+    let abierto = false;
+
+    function toggleCarta(){
+        if(!abierto){
+            $('.card').stop().animate({ top: '-90px' }, 500);
+            abierto = true;
+        }else{
+            $('.card').stop().animate({ top: '0px' }, 500);
+            abierto = false;
+        }
+    }
+
+    // PC (mouse)
+    $('.valentines').on('mouseenter', function(){
+        $('.card').stop().animate({ top: '-90px' }, 500);
     });
+
+    $('.valentines').on('mouseleave', function(){
+        $('.card').stop().animate({ top: '0px' }, 500);
+    });
+
+    // CELULAR (TOUCH)
+    $('.valentines').on('touchstart click', function(e){
+        e.preventDefault();
+        toggleCarta();
+    });
+
 });
